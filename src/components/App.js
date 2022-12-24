b3b4e2f7575bdea5aa79429ba1c369d145399f50
Namanvicky <116458366+Namanvicky@users.noreply.github.com>
2022-12-24 19:27:07 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -8,11 +8,11 @@ const ACTIONS = {
   SET_SUB_NUM: "setSubNum",
   SET_ADD_NUM: "setAddNum"
 };
-const initialstate={
+const initialState={
   count:10,
   subNum:1,
   addNum:1
-};
+}
 function reducer(state, action) {
   switch (action.type) {
     case ACTIONS.INCREMENT:
@@ -21,12 +21,12 @@ function reducer(state, action) {
         addNum: state.addNum,
         subNum: state.subNum
       };
-        case ACTIONS.DECREMENT:
-      return {
-        count: Number(state.count) + Number(state.addNum),
-        addNum: state.addNum,
-        subNum: state.subNum
-      };
+      case ACTIONS.DECREMENT:
+        return {
+          count: Number(state.count) - Number(state.subNum),
+          addNum: state.addNum,
+          subNum: state.subNum
+        };
  
    
     case ACTIONS.SET_ADD_NUM:
@@ -35,11 +35,11 @@ function reducer(state, action) {
         subNum: state.subNum,
         addNum: action.payload
       };
-       case ACTIONS.SET_SUB_NUM:
+    case ACTIONS.SET_SUB_NUM:
       return {
         count: state.count,
         subNum: action.payload,
-        addNum: state.subNum
+        addNum: state.addNum
       };
     default:
       return {
@@ -52,7 +52,7 @@ function reducer(state, action) {
 
 const App = () => {
  
-  const [state,dispatch]=useReducer(reducer,initialState);
+const [state, dispatch]=useReducer(reducer,initialState);
   function onIncrement() {
     dispatch({ type: ACTIONS.INCREMENT });
   }
